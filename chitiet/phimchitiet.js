a7f4efa45f64827b108b3cb3d04ea281d183dad6
quocthai2612003 <92153923+quocthai2612003@users.noreply.github.com>
2022-12-06 21:23:56 +0700

    update
    update file

--- chitiet/phimchitiet.js
+++ chitiet/phimchitiet.js
@@ -1,32 +1,112 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.profile-text-container,.toggle,.menu-list-container,.menu-list"
+);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+window.addEventListener("load", function () {
+  const slider = document.querySelector(".slider");
+  const sliderMain = document.querySelector(".slider-main");
+  const sliderItem = document.querySelectorAll(".slider-item");
+  const nextBtn = document.querySelector(".arrow-right");
+  const prevBtn = document.querySelector(".arrow-left");
+  const sliderItemWidth = sliderItem[0].offsetWidth;
+  const slidesLength = sliderItem.length;
+  let positionX = 0;
+  let index = 0;
+  nextBtn.addEventListener("click", function () {
+    handlechangeSlide(1);
+  });
+  prevBtn.addEventListener("click", function () {
+    handlechangeSlide(-1);
+  });
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+  function handlechangeSlide(direction) {
+    if (direction === 1) {
+      if (index >= slidesLength - 1) {
+        index = slidesLength - 1;
+        return;
+      }
+      positionX = positionX - sliderItemWidth;
+      console.log("handlechangeSlide ~ positionX", positionX);
+      sliderMain.style = `transform: translateX(${positionX}px)`;
+      index++;
+    } else if (direction === -1) {
+      if (index <= 0) {
+        index = 0;
+        return;
+      }
+      positionX = positionX + sliderItemWidth;
+      sliderMain.style = `transform: translateX(${positionX}px)`;
+      index--;
+    }
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+});
+
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumer = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumer - (4 + clickCounter) >= 0) {
+      // if(itemNumer - (4+clickCounter)+(4-ratio) >= 0){
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(movieLists[i].querySelectorAll("img").length);
+});
+
+window.addEventListener("load", function () {
+  const slider = document.querySelector(".movie-list-wrapper-top1");
+  const sliderMain = document.querySelector(".list1");
+  const sliderItem = document.querySelectorAll(".featured-content1");
+  const nextBtn = document.querySelector(".arrowright1");
+  const prevBtn = document.querySelector(".arrowleft1");
+  const sliderItemWidth = sliderItem[0].offsetWidth;
+  const slidesLength = sliderItem.length;
+  let positionX = 0;
+  let index = 0;
+  nextBtn.addEventListener("click", function () {
+    handlechangeSlide(1);
+  });
+  prevBtn.addEventListener("click", function () {
+    handlechangeSlide(-1);
+  });
+
+  function handlechangeSlide(direction) {
+    if (direction == 1) {
+      if (index >= slidesLength - 1) {
+        index = slidesLength - 1;
+        return;
+      }
+      positionX = positionX - sliderItemWidth;
+      console.log("handlechangeSlide ~ positionX", positionX);
+      sliderMain.style = `transform: translateX(${positionX}px)`;
+      index++;
+    } else if (direction == -1) {
+      if (index <= 0) {
+        index = 0;
+        return;
+      }
+      positionX = positionX + sliderItemWidth;
+      sliderMain.style = `transform: translateX(${positionX}px)`;
+      index--;
+    }
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+});
